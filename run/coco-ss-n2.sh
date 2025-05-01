cbd263fa36e50100a7741e55afe0bf37702912bb
XihuaQiao <1924494629@qq.com>
2025-05-01 23:34:59 +0800

    2025-05-01

--- run/coco-ss-n2.sh
+++ run/coco-ss-n2.sh
@@ -3,7 +3,7 @@
 export CUDA_VISIBLE_DEVICES=$1
 port=$(python get_free_port.py)
 echo ${port}
-alias exp="python -m torch.distributed.launch --master_port ${port} --nproc_per_node=4 run.py --num_workers 4"
+alias exp="python -m torch.distributed.launch --master_port ${port} --nproc_per_node=2 run.py --num_workers 4"
 shopt -s expand_aliases
 
 ds=coco
@@ -12,18 +12,18 @@ name=$3
 extra=$4
 
 # exp --method FT --name FT --epochs 20 --lr 0.01 --batch_size 24
-exp --method COS --name ${name} --epochs 30 --lr 0.01 --batch_size 6 --task ${task} --dataset ${ds} --val_interval 5 ${extra}
+# exp --method COS --name ${name} --epochs 30 --lr 0.01 --batch_size 6 --task ${task} --dataset ${ds} --val_interval 5 ${extra}
 # exp --method SPN --name SPN --epochs 20 --lr 0.01 --batch_size 24
 # exp --method DWI --name DWI --epochs 20 --lr 0.01 --batch_size 24 --ckpt checkpoints/step/${task}-voc/COS_0.pth
 # exp --method RT --name RT --epochs 40 --lr 0.01 --batch_size 24 --ckpt checkpoints/step/${task}-voc/FT_0.pth --born_again
 
-gen_par="--task ${task} --dataset ${ds} --batch_size 10"
-lr=0.001
+gen_par="--task ${task} --dataset ${ds} --batch_size 5"
+lr=0.005
 iter=2000
 path=checkpoints/step/${task}-${ds}
-for ns in 1 2 5; do  # shot 1/2/5 images
+for ns in 2; do  # shot 1/2/5 images
   for is in 0 1 2; do  # image samples
-    inc_par="--ishot ${is} --input_mix novel --val_interval 1000 --ckpt_interval 5"
+    inc_par="--ishot ${is} --input_mix novel --val_interval 10 --ckpt_interval 5"
 
       # exp --method FT --name FT --iter ${iter} --lr ${lr} ${gen_par} ${inc_par} --step 1 --nshot ${ns} --step_ckpt ${path}/FT_0.pth
 
@@ -38,6 +38,6 @@ for ns in 1 2 5; do  # shot 1/2/5 images
       # exp --method ILT --name ILT --iter ${iter} --lr ${lr} ${gen_par} ${inc_par} --step 1 --nshot ${ns} --step_ckpt ${path}/FT_0.pth
       # exp --method MIB --name MIB --iter ${iter} --lr ${lr} ${gen_par} ${inc_par} --step 1 --nshot ${ns} --step_ckpt ${path}/FT_0.pth
 
-      # exp --method PIFS --name PIFS --iter ${iter} --lr ${lr} ${gen_par} ${inc_par} --step 1 --nshot ${ns} --step_ckpt ${path}/COS-mixup4_0.pth
+      exp --method PIFS --name ${name} --iter ${iter} --lr ${lr} ${gen_par} ${inc_par} --step 1 --nshot ${ns} --step_ckpt ${path}/COS-mixup4_0.pth ${extra}
   done
 done
